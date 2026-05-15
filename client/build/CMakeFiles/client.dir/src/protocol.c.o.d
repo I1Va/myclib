d64b5c77426f685662434a7b3d78104231d67d61
ivan_a <mai.abrutinid@gmail.com>
2026-05-15 21:37:20 +0300

    fix: clearing

--- client/build/CMakeFiles/client.dir/src/protocol.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/client.dir/src/protocol.c.o: \
- /home/iabrutin/Work/Ded/sem4/temp/simple_stdlib/client/src/protocol.c \
- /home/iabrutin/Work/Ded/sem4/temp/simple_stdlib/client/inc/protocol.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stddef.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdint.h \
- /usr/lib/gcc/x86_64-pc-linux-gnu/16.1.1/include/stdint-gcc.h \
- /home/iabrutin/Work/Ded/sem4/temp/simple_stdlib/client/myclib/inc/string.h
